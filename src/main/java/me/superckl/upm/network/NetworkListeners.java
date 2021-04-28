87470432e05f4d8431e880263a614cbc21347e8a
super <superckl@superckl.me>
2021-04-28 17:55:56 -0500

    Also update networks if a new network members is placed next to them

--- src/main/java/me/superckl/upm/network/NetworkListeners.java
+++ src/main/java/me/superckl/upm/network/NetworkListeners.java
@@ -1,6 +1,14 @@
 package me.superckl.upm.network;
 
+import java.util.Collections;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
 import me.superckl.upm.UPMTile;
+import me.superckl.upm.network.member.NetworkMember;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.WorldTickEvent;
 import net.minecraftforge.event.world.BlockEvent;
@@ -9,21 +17,36 @@ import net.minecraftforge.fml.LogicalSide;
 
 public class NetworkListeners {
 
+	/**
+	 * Event listener to update networks when blocks in or next to the network are updated
+	 */
 	@SubscribeEvent
 	public void onBlockChange(final BlockEvent.NeighborNotifyEvent e) {
 		if(e.getWorld().isClientSide())
 			return;
-		//TODO listen for updates next to the network and that the added block has a valid capability
+		//Initialize the set with the changed block's positions to check if it
+		//was in a network
+		Set<BlockPos> updated = Sets.newHashSet(e.getPos());
+		//If the updated block has sided network members, check for networks on those sides as well
+		TileEntity teUpdated = e.getWorld().getBlockEntity(e.getPos());
+		if(teUpdated != null) {
+			e.getNotifiedSides().forEach(dir -> NetworkMember.from(teUpdated, dir)
+					.ifPresent(member -> updated.add(e.getPos().relative(dir))));
+		}
 		UPMTile.LOADED_TILES.stream().filter(tile -> tile.getNetwork() != null).forEach(tile -> {
+			//For each loaded network, test if it contains any of the updated positions and rescan if so
 			final EnergyNetwork network = tile.getNetwork();
-			if(network.getMembers().stream().anyMatch(wrapped -> wrapped.getPositions().containsKey(e.getPos()))) {
+			if(network.getMembers().stream().anyMatch(wrapped -> !Collections.disjoint(wrapped.getPositions().keySet(), updated))) {
 				network.scan();
-				network.getOwner().resetScanDelay();
+				tile.resetScanDelay();
 				tile.syncToClientLight(null);
 			}
 		});
 	}
 
+	/**
+	 * Event listener to tick loaded networks once world ticking is done
+	 */
 	@SubscribeEvent
 	public void onWorldTick(final WorldTickEvent e) {
 		if(e.side == LogicalSide.SERVER && e.phase == Phase.END)
