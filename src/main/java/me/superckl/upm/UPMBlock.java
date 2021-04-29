86f2c927dfe142b4eb5e44c82d6de77a3a4613ee
super <superckl@superckl.me>
2021-04-28 19:43:40 -0500

    Add block texture

--- src/main/java/me/superckl/upm/UPMBlock.java
+++ src/main/java/me/superckl/upm/UPMBlock.java
@@ -5,10 +5,13 @@ import me.superckl.upm.packet.UPMPacketHandler;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -17,7 +20,7 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.PacketDistributor;
 
-public class UPMBlock extends Block{
+public class UPMBlock extends HorizontalBlock{
 
 	public UPMBlock() {
 		super(AbstractBlock.Properties.of(Material.METAL).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL));
@@ -32,6 +35,10 @@ public class UPMBlock extends Block{
 	public UPMTile createTileEntity(final BlockState state, final IBlockReader world) {
 		return new UPMTile();
 	}
+	
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+	      return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+	   }
 
 	@Override
 	public ActionResultType use(final BlockState state, final World level, final BlockPos pos,
@@ -40,6 +47,12 @@ public class UPMBlock extends Block{
 			UPMPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player),
 					new OpenUPMScreenPacket(pos));
 		return ActionResultType.sidedSuccess(level.isClientSide);
+	}
+	
+	@Override
+	protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
+		builder.add(HorizontalBlock.FACING);
+		super.createBlockStateDefinition(builder);
 	}
 
 }
