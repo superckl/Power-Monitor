da6c20c830fcb6b356da829f73cc59469fb2049b
super <superckl@superckl.me>
2021-05-12 19:43:20 -0500

    Add IF tile tags

--- src/main/java/me/superckl/upm/data/TileTypeTagGenerator.java
+++ src/main/java/me/superckl/upm/data/TileTypeTagGenerator.java
@@ -33,6 +33,9 @@ public class TileTypeTagGenerator extends ForgeRegistryTagsProvider<TileEntityTy
 			this.addThermal(builder, type);
 			this.addBiggerReactors(builder, type);
 			this.addPowah(builder, type);
+			this.addRFTools(builder, type);
+			//IF has a ton of machines but there's no good way to easily grab them from the mod source :/
+			this.addIndustrialForegoing(builder, type);
 
 			//IE is handled in its integration because it has a lot and uses a resolver
 			//Mekanism is handled in its integration because it has an inane amount of tiles
@@ -73,7 +76,6 @@ public class TileTypeTagGenerator extends ForgeRegistryTagsProvider<TileEntityTy
 			break;
 		default:
 			break;
-
 		}
 	}
 
@@ -88,7 +90,6 @@ public class TileTypeTagGenerator extends ForgeRegistryTagsProvider<TileEntityTy
 			builder.addOptional(new ResourceLocation(reactorsName, "cyanite_reprocessor"));
 		default:
 			break;
-
 		}
 	}
 
@@ -116,6 +117,85 @@ public class TileTypeTagGenerator extends ForgeRegistryTagsProvider<TileEntityTy
 			builder.addOptional(new ResourceLocation(powahName, "furnator"));
 			builder.addOptional(new ResourceLocation(powahName, "magmator"));
 			builder.addOptional(new ResourceLocation(powahName, "thermo_gen"));
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void addRFTools(final Builder<TileEntityType<?>> builder, final MemberType type) {
+		final String builderName = "rftoolsbuilder";
+		switch(type) {
+		case MACHINE:
+			builder.addOptional(new ResourceLocation(builderName, "builder"));
+			builder.addOptional(new ResourceLocation(builderName, "shield_block1"));
+			builder.addOptional(new ResourceLocation(builderName, "shield_block2"));
+			builder.addOptional(new ResourceLocation(builderName, "shield_block3"));
+			builder.addOptional(new ResourceLocation(builderName, "shield_block4"));
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void addIndustrialForegoing(final Builder<TileEntityType<?>> builder, final MemberType type) {
+		final String name = "industrialforegoing";
+		switch(type) {
+		case GENERATOR:
+			builder.addOptional(new ResourceLocation(name, "pitiful_generator"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_reactor"));
+			builder.addOptional(new ResourceLocation(name, "biofuel_generator"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_furnace"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_slimey"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_culinary"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_potion"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_disenchantment"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_ender"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_explosive"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_frosty"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_halitosis"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_magma"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_pink"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_netherstar"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_death"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_rocket"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_crimed"));
+			builder.addOptional(new ResourceLocation(name, "mycelial_generator_meatallurgic"));
+			builder.addOptional(new ResourceLocation(name, "bioreactor"));
+			break;
+		case MACHINE:
+			builder.addOptional(new ResourceLocation(name, "animal_rancher"));
+			builder.addOptional(new ResourceLocation(name, "hydroponic_bed"));
+			builder.addOptional(new ResourceLocation(name, "plant_gatherer"));
+			builder.addOptional(new ResourceLocation(name, "plant_sower"));
+			builder.addOptional(new ResourceLocation(name, "plant_fertilizer"));
+			builder.addOptional(new ResourceLocation(name, "animal_feeder"));
+			builder.addOptional(new ResourceLocation(name, "mob_crusher"));
+			builder.addOptional(new ResourceLocation(name, "mob_duplicator"));
+			builder.addOptional(new ResourceLocation(name, "wither_builder"));
+			builder.addOptional(new ResourceLocation(name, "mob_slaughter_factory"));
+			builder.addOptional(new ResourceLocation(name, "sewer"));
+			builder.addOptional(new ResourceLocation(name, "sewage_composter"));
+			builder.addOptional(new ResourceLocation(name, "animal_baby_separator"));
+			builder.addOptional(new ResourceLocation(name, "washing_factory"));
+			builder.addOptional(new ResourceLocation(name, "material_stonework_factory"));
+			builder.addOptional(new ResourceLocation(name, "fluid_sieving_machine"));
+			builder.addOptional(new ResourceLocation(name, "mechanical_dirt"));
+			builder.addOptional(new ResourceLocation(name, "block_breaker"));
+			builder.addOptional(new ResourceLocation(name, "fermentation_station"));
+			builder.addOptional(new ResourceLocation(name, "marine_fisher"));
+			builder.addOptional(new ResourceLocation(name, "fluid_placer"));
+			builder.addOptional(new ResourceLocation(name, "block_placer"));
+			builder.addOptional(new ResourceLocation(name, "resourceful_furnace"));
+			builder.addOptional(new ResourceLocation(name, "ore_laser_base"));
+			builder.addOptional(new ResourceLocation(name, "fluid_collector"));
+			builder.addOptional(new ResourceLocation(name, "spores_recreator"));
+			builder.addOptional(new ResourceLocation(name, "potion_brewer"));
+			builder.addOptional(new ResourceLocation(name, "sludge_refiner"));
+			builder.addOptional(new ResourceLocation(name, "water_condensator"));
+			builder.addOptional(new ResourceLocation(name, "dye_mixer"));
+			builder.addOptional(new ResourceLocation(name, "laser_drill"));
+			builder.addOptional(new ResourceLocation(name, "fluid_laser_base"));
 			break;
 		default:
 			break;
